cea515e3e62a07152557f26f6e1631bacbec41e8
isaacalderonCETYS <isaac.calderon@cetys.edu.mx>
2021-03-16 20:37:26 -0700

    Initial Commit

---
+++ src/main/java/com/dessierto/dphdessierto/DphDessiertoApplication.java
@@ -0,0 +1,13 @@
+package com.dessierto.dphdessierto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DphDessiertoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DphDessiertoApplication.class, args);
+	}
+
+}
